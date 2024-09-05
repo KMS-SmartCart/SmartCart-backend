a88808e9758302c7dd92f230dc5795d220d6be22
sahyunjin <tkguswls1106@naver.com>
2024-09-06 02:35:26 +0900

    Fix: ProductService impl 문제 수정

--- src/main/java/com/kms/smartcart_backend/service/impl/ProductServiceImpl.java
+++ src/main/java/com/kms/smartcart_backend/service/impl/ProductServiceImpl.java
@@ -1,12 +1,13 @@
 package com.kms.smartcart_backend.service.impl;
 
 import com.kms.smartcart_backend.repository.ProductRepository;
+import com.kms.smartcart_backend.service.ProductService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
-public class ProductServiceImpl {
+public class ProductServiceImpl implements ProductService {
 
     private final ProductRepository productRepository;
 
