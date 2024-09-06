7e0cded47a075dc70f44c2699d0c9c840de6a288
sahyunjin <tkguswls1106@naver.com>
2024-09-06 20:04:06 +0900

    Fix: updateUser PUT Method로 변경

--- src/main/java/com/kms/smartcart_backend/controller/UserController.java
+++ src/main/java/com/kms/smartcart_backend/controller/UserController.java
@@ -8,7 +8,7 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,7 +20,7 @@ public class UserController {
     private final UserService userService;
 
 
-    @PostMapping("/users")
+    @PutMapping("/users")
     @Operation(summary = "내정보 Page - 회원 닉네임 변경 or 아낀 총 금액 추가 [JWT O]",
             description = """
                 - <strong>회원 닉네임 변경</strong> : nickname = 변경할 닉네임 , savedMoney = null
@@ -28,7 +28,7 @@ public class UserController {
                 - <strong>ERROR 1</strong> : nickname = 변경할 닉네임 , savedMoney = 새롭게 아낀 금액
                 - <strong>ERROR 2</strong> : nickname = null , savedMoney = null
                 """)
-    public ResponseEntity<ResponseData> createRoom(@RequestBody UserDto.UpdateRequest updateRequestDto) {
+    public ResponseEntity<ResponseData> updateUser(@RequestBody UserDto.UpdateRequest updateRequestDto) {
         userService.updateUser(updateRequestDto);
         return ResponseData.toResponseEntity(ResponseCode.UPDATE_USER);
     }
