d989bf0688082473c655d0d26e24e45e6acb410d
sahyunjin <tkguswls1106@naver.com>
2024-09-23 21:12:30 +0900

    Setting: OAuth2 리다이렉트 개발버전으로 전환

--- src/main/java/com/kms/smartcart_backend/security/oauth2/handler/OAuth2LoginFailureHandler.java
+++ src/main/java/com/kms/smartcart_backend/security/oauth2/handler/OAuth2LoginFailureHandler.java
@@ -19,7 +19,7 @@ public class OAuth2LoginFailureHandler extends SimpleUrlAuthenticationFailureHan
     public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response, AuthenticationException exception) throws IOException, ServletException {
         log.info("OAuth2 소셜 로그인에 실패했습니다. 로그인 방식 선택 페이지로 리다이렉트 시킵니다. => 에러 메시지 : {}", exception.getMessage());
 
-        String frontendUrl = "https://www.smartcart.kr";  // "http://localhost:3000"
+        String frontendUrl = "http://localhost:3000";  // "https://www.smartcart.kr"
         frontendUrl += "/login";
         String redirectUrl = UriComponentsBuilder.fromUriString(frontendUrl)
                 .build().toUriString();
